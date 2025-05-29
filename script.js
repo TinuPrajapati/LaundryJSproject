4e8d07df3f6df93416305fbb6012c1888c170c86
Tinu Prajapati <tinup2580@gmail.com>
2025-05-29 21:43:03 +0530

    MERGE

--- script.js
+++ script.js
@@ -1,11 +1,5 @@
-document.addEventListener("onmousemove", (event) => {
-  const circle = document.getElementById("mouse");
-  circle.style.left = `${event.pageX}px`;
-  circle.style.top = `${event.pageY}px`;
-});
-
-
-  let totalPrice = 0;
+const Price = document.getElementById('cart-total').innerText;
+let totalPrice = 0;
   let itemCount = 0;
   const cartList = document.getElementById('cart-list');
   const cartTotal = document.getElementById('cart-total');
@@ -48,17 +42,23 @@ document.addEventListener("onmousemove", (event) => {
   });
   
   const mssg = document.getElementById('mssg')
-  document.getElementsByClassName('book-now').addEventListener("submit",(e) =>{
-    e.preventDefault();
-    mssg.style.display= "block";
-    emails();
+  const submit = document.querySelector('.book-now')
+  submit.addEventListener('click', (e)=>{
+    e.preventDefault()
+    if(itemCount === 0){
+      alert("Please add items to the cart")
+      return
+    }
+    console.log("clicked")
+    emails()
+    mssg.style.display = "block"
   })
 
   function emails(){
     const template = {
-      name: document.getElementById("#name").value,
+      name: document.getElementById("name").value,
       email: document.querySelector('#email-id').value,
-      phone: document.querySelector("#phone-id").value
+      phone: document.querySelector("#phone-no").value
     }
-    emailjs.send(service_agyio3d,template_8ujd6up,template).then(()=> alert("email sent!").catch(()=> alert("email not send!")))
+    emailjs.send(service_agyio3d,"template_8ujd6up",template).then(()=> alert("email sent!").catch(()=> alert("email not send!")))
   }
